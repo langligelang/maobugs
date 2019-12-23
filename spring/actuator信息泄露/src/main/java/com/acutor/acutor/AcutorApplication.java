5c4c6292b727c39ec5c53b8e0071f637d42b4794
langlige123@hotmail.com <1039607355abc>
2019-12-23 12:05:09 +0800

    springboot actuator信息泄露

---
+++ spring/actuator信息泄露/src/main/java/com/acutor/acutor/AcutorApplication.java
@@ -0,0 +1,13 @@
+package com.acutor.acutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcutorApplication.class, args);
+    }
+
+}
